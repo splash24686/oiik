dd9cac3c46ce0b7ff84fb4da8a9fa9cf32e3d342
FRIX_Y <73866768+splash24686@users.noreply.github.com>
2021-10-10 15:52:19 +0530

    Update start.bat

--- PhoenixMiner-5.5c/start.bat
+++ PhoenixMiner-5.5c/start.bat
@@ -1,3 +1,3 @@
 @echo off
-PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.mb4u18hz3qewrnx -gpow 96
+PhoenixMiner.exe -pool ssl://us1-etc.ethermine.org:5555 -pool2 ssl://eu1-etc.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.2gs2d6jahqzg0qq -gpow 96
 pause
